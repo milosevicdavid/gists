7873a8837d59bc8d70be7de9ecf4731a1fcb2372
David Milosevic <milosevicdavid@gmail.com>
2021-10-02 20:55:07 +0200

    gists

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,125 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="app">
+      <div>
+        <h1 class="title">Github gists</h1>
+        <div v-if="loads" class="spinner-loader">
+          <p class="loading">Loading...</p>
+          <rotate-square3></rotate-square3>
+        </div>
+        <div v-else class="items-buttons">
+          <div class="buttons">
+            <button @click="getItems(1)">1</button>
+            <button @click="getItems(2)">2</button>
+          </div>
+          <Gist v-for="item in gist" :key="item.id" :item="item" />
+          <div class="buttons">
+            <button @click="getItems(1)">1</button>
+            <button @click="getItems(2)">2</button>
+            <button @click="windowTop">↑</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Gist from "./components/Gist.vue";
+import { RotateSquare3 } from "vue-loading-spinner";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Gist,
+    RotateSquare3
+  },
+  data() {
+    return {
+      gist: [],
+      page: "",
+      loads: false,
+      click: 1
+    };
+  },
+
+  mounted() {
+    this.getItems();
+  },
+
+  methods: {
+    getItems(num = 1) {
+      this.loads = true;
+      fetch(`https://api.github.com/gists/public?page=${num}`)
+        .then(response => response.json())
+        .then(data => (this.gist = data))
+        .then(() => (this.loads = false));
+
+      this.windowTop();
+    },
+    windowTop() {
+      window.scroll(0, 0);
+    }
   }
-}
+};
 </script>
 
 <style>
+html {
+  scroll-behavior: smooth;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.buttons button {
+  margin: 20px 5px;
+  color: #177ef2;
+  width: 40px;
+  height: 40px;
+  border: 1px solid #177ef2;
+  background-color: white;
+  cursor: pointer;
+}
+
+.buttons button:hover {
+  color: gray;
+  border: 1px solid gray;
+}
+
+.spinner-loader {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 200px;
+  margin-top: 200px;
+  font-size: 24px;
+}
+
+.spinner {
+  width: 80px !important;
+  height: 80px !important;
+}
+
+.loading {
+  color: #4fc08d;
+}
+
+.items-buttons {
+  margin-top: 120px;
 }
 </style>
---
+++ src/components/Gist.vue
@@ -0,0 +1,100 @@
+<template>
+  <div
+    @click="changeId(item.id)"
+    :class="['item-wrapper', { 'item-click-text': click === item.id }]"
+  >
+    <div class="avatar-wrapper">
+      <img
+        :src="item.owner.avatar_url"
+        alt="avatar"
+        :class="['avatar', { 'item-click-avatar': click === item.id }]"
+      />
+    </div>
+    <div class="file-name">
+      {{ Object.keys(item.files)[0] }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      click: -1
+    };
+  },
+
+  methods: {
+    changeId(id) {
+      this.click = id;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.avatar {
+  width: 150px;
+  height: 150px;
+}
+
+.item-click-text {
+  color: #177ef2;
+}
+
+.item-click-avatar {
+  -webkit-filter: grayscale(50%) brightness(51%);
+  animation: linear;
+  animation-name: movefade;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+}
+
+.item-wrapper {
+  border-bottom: 1px solid lightgray;
+  padding: 30px 0px;
+  display: flex;
+  justify-content: flex-start;
+  width: 900px;
+  cursor: pointer;
+}
+
+.item-wrapper:nth-child(2) {
+  border-top: 1px solid lightgray;
+}
+
+.file-name {
+  margin-left: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.avatar-wrapper {
+}
+
+@keyframes movefade {
+  0% {
+    transform: translateX(0%);
+    opacity: 1;
+  }
+  50% {
+    transform: translateX(250%);
+    opacity: 1;
+  }
+  80% {
+    transform: translateX(250%);
+    opacity: 0.5;
+  }
+  100% {
+    transform: translateX(250%);
+    opacity: 0;
+  }
+}
+</style>
